7c088d6a3bbcc48eb96271e7b90da0a05c11e1b4
pradow <pradochris+pradow@protonmail.com>
2021-09-17 08:11:07 -0700

    add page folder components

---
+++ src/Home/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import ProductCard from "./ProductCard";
+
+export function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <ProductCard />
+    </div>
+  );
+}
+
+export function HomeTwo() {
+  return (
+    <div>
+      <h2>Home 2</h2>
+    </div>
+  );
+}
