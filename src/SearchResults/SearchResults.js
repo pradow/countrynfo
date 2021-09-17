80826739c56c2dc97e33095541096be5ee5d3149
pradow <pradochris+pradow@protonmail.com>
2021-09-17 12:12:13 -0700

    add initial statees for loading and data fetch

--- src/SearchResults/SearchResults.js
+++ src/SearchResults/SearchResults.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
+const url = "https://restcountries.eu/rest/v2/name/";
 export function SearchResults() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchData, setSearchData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setIsLoading(false);
+      setSearchData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(searchData);
+
+  if (isLoading) {
+    return (
+      <div>
+        <h3>...Loading</h3>
+      </div>
+    );
+  }
+
   return (
     <div>
       <h2>SearchResults</h2>
