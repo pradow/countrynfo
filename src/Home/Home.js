7e247132e45cc0df3eb97bdbef91ec10502209d0
pradow <pradochris+pradow@protonmail.com>
2021-09-17 11:39:53 -0700

    create search shared component and add initial handle functions

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
-
 import ProductCard from "./ProductCard";
+import Search from "../shared/Search";
 
 export function Home() {
   return (
     <div>
       <h1>Home Page</h1>
       <ProductCard />
+      <Search />
     </div>
   );
 }
